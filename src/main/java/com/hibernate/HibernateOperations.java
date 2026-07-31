1be6e89e124a2317d36c6c0ef58534e7ebd6f639
Vamshi-java <konkatavamshi95@gmail.com>
2026-07-31 21:59:33 +0530

    feature: insert operation using hibernate

---
+++ src/main/java/com/hibernate/HibernateOperations.java
@@ -0,0 +1,29 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.entity.Students;
+
+public class HibernateOperations {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Students.class);
+		
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		Students student = new Students(2,"kvs",29);
+		
+		session.beginTransaction();
+		session.persist(student);
+		session.getTransaction().commit();
+		
+		
+	}
+}
---
+++ src/main/java/com/hibernate/entity/Students.java
@@ -0,0 +1,64 @@
+package com.hibernate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Students {
+
+	@Id
+	private int id;
+	
+	private String name;
+	
+	private int age;
+
+	public Students(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public Students(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public Students() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Students [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
+	
+}
